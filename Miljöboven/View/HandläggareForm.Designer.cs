91f2cb635efb513a39312889a92ce5351f74d9d1
12jenras <rasmus-andreas@hotmail.com>
2015-01-19 11:21:48 +0100

    ghjgh

--- Miljöboven/View/HandläggareForm.Designer.cs
+++ Miljöboven/View/HandläggareForm.Designer.cs
@@ -34,43 +34,44 @@
             this.btnSkicka = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // clbÄrendetitlar
             // 
             this.clbÄrendetitlar.FormattingEnabled = true;
             this.clbÄrendetitlar.Items.AddRange(new object[] {
-            "Titel 1",
+            "Databygge med miljöproblem",
             "Titel 2",
             "Titel 3"});
             this.clbÄrendetitlar.Location = new System.Drawing.Point(12, 12);
             this.clbÄrendetitlar.Name = "clbÄrendetitlar";
-            this.clbÄrendetitlar.Size = new System.Drawing.Size(251, 274);
+            this.clbÄrendetitlar.Size = new System.Drawing.Size(171, 274);
             this.clbÄrendetitlar.TabIndex = 0;
             // 
             // rtbÄrenedeinformation
             // 
-            this.rtbÄrenedeinformation.Location = new System.Drawing.Point(269, 12);
+            this.rtbÄrenedeinformation.Location = new System.Drawing.Point(189, 188);
             this.rtbÄrenedeinformation.Name = "rtbÄrenedeinformation";
-            this.rtbÄrenedeinformation.Size = new System.Drawing.Size(193, 170);
+            this.rtbÄrenedeinformation.Size = new System.Drawing.Size(170, 98);
             this.rtbÄrenedeinformation.TabIndex = 1;
-            this.rtbÄrenedeinformation.Text = "1/1/2015 Blev tilldelat ärendet. Har ringt markägaren \noch pratat med denna.\n\n2/1" +
-    "/2015 Åker till brottsplatsen och undersöker.";
+            this.rtbÄrenedeinformation.Text = "Info om ärendet\n";
             // 
             // btnSkicka
             // 
-            this.btnSkicka.Location = new System.Drawing.Point(622, 253);
+            this.btnSkicka.Location = new System.Drawing.Point(619, 214);
             this.btnSkicka.Name = "btnSkicka";
             this.btnSkicka.Size = new System.Drawing.Size(105, 33);
             this.btnSkicka.TabIndex = 2;
-            this.btnSkicka.Text = "Skicka";
+            this.btnSkicka.Text = "Kommentera";
             this.btnSkicka.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(269, 185);
+            this.label1.Location = new System.Drawing.Point(365, 188);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(108, 23);
             this.label1.TabIndex = 3;
@@ -78,17 +79,38 @@
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(273, 214);
+            this.richTextBox1.Location = new System.Drawing.Point(369, 214);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(343, 72);
+            this.richTextBox1.Size = new System.Drawing.Size(244, 72);
             this.richTextBox1.TabIndex = 4;
             this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(619, 253);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 33);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Slutför";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "21 / 1 - 2015   Besökte platsen och fixade så att man kan se ifall det är bra"});
+            this.listBox1.Location = new System.Drawing.Point(189, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(535, 160);
+            this.listBox1.TabIndex = 6;
             // 
             // HandläggareForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(736, 303);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnSkicka);
@@ -98,7 +120,6 @@
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "HandläggareForm";
-            this.Text = "-";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.HandläggareForm_FormClosing);
             this.Load += new System.EventHandler(this.HandläggareForm_Load);
             this.ResumeLayout(false);
@@ -113,5 +134,7 @@
         private System.Windows.Forms.Button btnSkicka;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
